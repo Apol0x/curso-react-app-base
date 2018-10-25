986fb1b8f5e48124e0b73c6ee1f053930228d9d7
Madeja Polo <madejapolo@gmail.com>
2018-10-26 01:44:31 +0200

    editing search components and index

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,8 +4,31 @@ import React, { PropTypes } from 'react';
  * Renderiza el formulario de búsqueda.
  */
 class SearchForm extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      search: '',
+    }
+  }
+
+  onChange = () => {
+    
+  }
+
+  onSubmit = () => {
+    
+  }
   render() {
-    return null;
+    return <div>
+        <label>Busca un repositorio</label>
+        <input type='text' className="u-full-width" placeholder="react webpack ...etc"
+        onChange={this.onChange()} defaultValue={this.state.search}/>
+        <p className="align-left">
+          <input type="submit" className="button-primary" value="Buscar"/> 
+        </p>
+      </div>
+    
   }
 }
 
--- src/containers/SearchContainer/SearchContainer.js
+++ src/containers/SearchContainer/SearchContainer.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
-
+import Header from '../../components/Header';
+import SearchForm from '../../components/SearchForm';
 // Importamos los componentes
 
 
@@ -7,6 +8,15 @@ import React, { PropTypes } from 'react';
  * Muestra un buscador, así como la lista de resultados.
  */
 class SearchContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      result: [],
+      search: '',
+      queried: false
+    }
+  }
   /**
    * Datos falsos. Los utilizamos en desarrollo hasta que leamos los datos de
    * la API.
@@ -36,14 +46,25 @@ class SearchContainer extends React.Component {
     ]
   }
 
-  /**
-   * Render the SearchContainer component
-   */
-  render() {
-    return <main className="container">
-      <h1>Búsqueda</h1>
-    </main>
+  onSubmit = (value) => {
+    this.state({ loadin: true });
+    setTimeout(() => {
+      this.State({ loadin: false, queried: true, result: this.stubData() });
+    }, 2000);
   }
+
+
+/**
+ * Render the SearchContainer component
+ * 
+ */
+render() {
+  return <main className="container">
+    <Header />
+    <SearchForm onSubmit={this.onSubmit} search={this.state.search} />
+    <h1>Búsqueda</h1>
+  </main >
+}
 }
 
 // Exportamos
